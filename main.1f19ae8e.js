8b0059c3b2e92786479f8ef721862aba4a545cdd
Nikolay Igotti <igotti@gmail.com>
2020-04-12 12:33:20 +0300

    Updates

--- main.1f19ae8e.js
+++ main.1f19ae8e.js
@@ -24686,10 +24686,11 @@ function _show_poll_results() {
             votedItem.innerText = Object.keys(response.results.voted).join(" ");
             newHolder.appendChild(votedItem);
             resultsForm = document.getElementById('poll-results-form');
+            window.console.log(resultsForm.firstChild);
             resultsForm.replaceChild(resultsForm.firstChild, newHolder);
             document.getElementById('vote-options').style.display = 'none';
 
-          case 26:
+          case 27:
           case "end":
             return _context4.stop();
         }
